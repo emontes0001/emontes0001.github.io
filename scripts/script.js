aa0a9637b144e83ad2d25e47d9b172a1d53eb18e
emontes0001 <156948198+emontes0001@users.noreply.github.com>
2024-05-01 20:41:46 -0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+/*
+	Student Name: Edwin Montes
+	File Name: scripts.js
+	Today's Date: 05/01/2024	
+*/
+
+// JavaScript to fade in the image
+window.addEventListener('load', function() {
+    document.getElementById('myImage').style.opacity = 1;
+});
+  
